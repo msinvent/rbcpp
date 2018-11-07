f19788a877cc1f5e0e1766b04585f5a8c946042f
juliangaal <gjulian@uos.de>
2018-11-07 12:33:47 +0100

    clean

---
+++ include/ros_bridge_client/msgs/geometry_msgs/twist.h
@@ -0,0 +1,46 @@
+//
+// Created by Julian on 18.09.18.
+//
+
+#ifndef ROSBRIDGECLIENT_ACCEL_HPP
+#define ROSBRIDGECLIENT_ACCEL_HPP
+
+#include <cpprest/json.h>
+#include <ros_bridge_client/msgs/message_base.h>
+#include <ros_bridge_client/msgs/geometry_msgs/vector3.h>
+#include <memory>
+#include <iostream>
+
+namespace ros_bridge_client
+{
+namespace msgs
+{
+namespace geometry_msgs
+{
+
+struct Accel : public MessageBase
+{
+  Accel(std::string ros_msg_type = "geometry_msgs/Accel");
+
+  Accel(double lx, double ly, double lz, double ax, double ay, double az, std::string ros_msg_type = "geometry_msgs/Accel");
+
+  Accel(const Vector3 &linear, const Vector3 &angular, std::string ros_msg_type = "geometry_msgs/Accel");
+
+  explicit Accel(const web::json::value &response, std::string ros_msg_type = "geometry_msgs/Accel");
+
+  ~Accel() override = default;
+
+  Vector3 linear;
+  Vector3 angular;
+};
+
+} // namespace geometry_msgs
+} // namespace msgs
+} // namespace ros_bridge_client
+
+std::ostream &operator<<(std::ostream &os, const ros_bridge_client::msgs::geometry_msgs::Accel &p);
+
+std::ostream &
+operator<<(std::ostream &os, const std::shared_ptr<ros_bridge_client::msgs::geometry_msgs::Accel> &p);
+
+#endif //ROSBRIDGECLIENT_ACCEL_HPP
---
+++ src/ros_bridge_client/msgs/geometry_msgs/twist.cxx
@@ -0,0 +1,54 @@
+//
+// Created by Julian on 29.10.18.
+//
+
+#include <ros_bridge_client/msgs/geometry_msgs/accel.h>
+#include <ros_bridge_client/utils/response_converter.h>
+
+using namespace ros_bridge_client::msgs::geometry_msgs;
+
+Accel::Accel(std::string frame_id)
+  : MessageBase(frame_id),
+    linear(0, 0, 0),
+    angular(0, 0, 0)
+{}
+
+Accel::Accel(double lx, double ly, double lz, double ax, double ay, double az, std::string frame_id)
+  : MessageBase(frame_id),
+    linear(lx, ly, lz),
+    angular(ax, ay, az)
+{}
+
+Accel::Accel(const Vector3 &linear, const Vector3 &angular, std::string frame_id)
+  : MessageBase(frame_id),
+    linear(linear),
+    angular(angular)
+{}
+
+Accel::Accel(const web::json::value &response, std::string frame_id)
+  : MessageBase(frame_id),
+    linear(0, 0, 0),
+    angular(0, 0, 0)
+{
+  const auto &msg = response.at(U("msg"));
+
+  std::tie(linear.x, linear.y, linear.z) =
+    utils::ResponseConverter::responseToVector3(msg.at(U("linear")), true);
+
+  std::tie(angular.x, angular.y, angular.z) =
+    utils::ResponseConverter::responseToVector3(msg.at(U("angular")), true);
+}
+
+std::ostream &operator<<(std::ostream &os, const ros_bridge_client::msgs::geometry_msgs::Accel &p)
+{
+  os << "\nAccel: linear/angular";
+  os << p.linear;
+  os << p.angular;
+  return os;
+}
+
+std::ostream & operator<<(std::ostream &os, const std::shared_ptr<ros_bridge_client::msgs::geometry_msgs::Accel> &p)
+{
+  return os << p.get();
+}
+

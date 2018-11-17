cde32f517f886ad5f02f8afc5b5f375c830a1ff4
Julian Gaal <gjulian@uos.de>
2018-11-17 21:28:12 +0100

    install script for unix

---
+++ scripts/deps.sh
@@ -0,0 +1,8 @@
+cd ~/Downloads
+
+git clone --recurse-submodules https://github.com/Microsoft/cpprestsdk && cd cpprestsdk
+mkdir build && cd build && cmake .. && make -j8 && sudo make install
+
+cd ~/Downloads
+git clone https://github.com/catchorg/Catch2 && cd Catch2
+mkdir build && cd build && cmake .. && make -j8 && sudo make install

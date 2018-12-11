81a47c9ad1aca669e034f332a145bdcf9c5b3e66
Julian Gaal <gjulian@uos.de>
2018-12-11 11:29:04 +0100

    correct pose array topic

--- src/ros_bridge_client/msgs/geometry_msgs/pose_array.cxx
+++ src/ros_bridge_client/msgs/geometry_msgs/pose_array.cxx
@@ -7,12 +7,10 @@
 using namespace ros_bridge_client::msgs::geometry_msgs;
 
 PoseArray::PoseArray(std::string frame_id)
-  : ROSTypeBase(frame_id),
-    header(),
+  : ROSTypeBase("geometry_msgs/PoseArray"),
+    header(frame_id),
     poses()
-{
-  poses.reserve(100);
-}
+{}
 
 void PoseArray::add(const Pose &pose)
 {
